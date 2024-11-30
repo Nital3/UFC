e28ae5de0fbce604094492e8a651acd9a43c2394
Nital <daniel.siwiaszczyk0@gmail.com>
2024-11-30 02:35:17 +0100

    test tacho fan branch

--- main.c
+++ main.c
@@ -1,38 +1,87 @@
-#include "lwip/apps/httpd.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
-#include "lwipopts.h"
-#include "hardware/adc.h"
 #include "hardware/pwm.h"
 
-#include "ssi.h"
-#include "cgi.h"
 
-//WIFI Creds
-const char WIFI_SSID[] = "HUAWEI";
-const char WIFI_PSWD[] = "10101010";
+#define GPIO_ON 1
+#define GPIO_OFF 0
+#define PWM_GPIO_PIN 0
+#define PULL_UP_PIN 15
+#define BUTTON_GPIO_PIN 16
 
 
 int main(){
 
-    stdio_init_all();
+    //Initialize wifi module and turn on LED to indicate program is loaded and working
     cyw43_arch_init();
-    cyw43_arch_enable_sta_mode();
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, GPIO_ON);
 
-    while (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PSWD, CYW43_AUTH_WPA2_AES_PSK, 30000))
-    {
-        printf("Connecting...");
+
+    //Set GPIO 14 pin to PWM mode
+    gpio_set_function(PWM_GPIO_PIN, GPIO_FUNC_PWM);
+
+    //Get PWM channel for that pin;
+    uint slice_number = pwm_gpio_to_slice_num(PWM_GPIO_PIN);
+
+    //Turn PWM on
+    pwm_set_enabled(slice_number, true);
+
+    //Set the PWM wrap point for 2kHz
+    int pwm_wrap_point = 62500;
+    pwm_set_wrap(slice_number, pwm_wrap_point);
+
+
+    //Mode_button setup
+    stdio_init_all();
+    gpio_init(BUTTON_GPIO_PIN);
+    gpio_set_dir(BUTTON_GPIO_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_GPIO_PIN);
+
+    //Test tacho pin
+    gpio_pull_up(PULL_UP_PIN);
+
+    int mode = 0;
+
+    for(;;){
+
+
+        while (gpio_get(BUTTON_GPIO_PIN)){
+
+            switch (mode){
+
+            case 0: //Brightness 0%
+                pwm_set_chan_level(slice_number, PWM_CHAN_A, 0);
+                break;
+
+            case 1: //Brightness 25%
+                pwm_set_chan_level(slice_number, PWM_CHAN_A, pwm_wrap_point*0.25f);
+                break;
+
+            case 2: //Brightness 50%
+                pwm_set_chan_level(slice_number, PWM_CHAN_A, pwm_wrap_point*0.5f);
+                break;
+
+            case 3: //Brightness 100%
+                pwm_set_chan_level(slice_number, PWM_CHAN_A, pwm_wrap_point);
+                break;
+
+            default:
+                break;
+
+            }
+
+        }
+        
+        if(mode<3){
+            mode++;
+        }
+        else{
+            mode = 0;
+        }
+
+        sleep_ms(250);
+
+
     }
-        printf("Connected!");
-
-    //Initialise html server
-    httpd_init();
-
-
-    ssi_init();
-    cgi_init();
-    
-
-    for(;;);
 
 }
--- ssi.c
+++ ssi.c
@@ -1,29 +1,63 @@
 #include "lwip/apps/httpd.h"
 #include "pico/cyw43_arch.h"
 #include "hardware/adc.h"
+#include "functions.h"
 
 #include "ssi.h"
 
 //Set SSI tags
-const char * ssi_tags[] = {"temp"};
+const char * ssi_tags[] = {"volt","temp","res","adc"};
 
 u16_t ssi_handler(int iIndex, char *pcInsert, int iInsertLen){
     size_t printed;
 
     switch ((iIndex))
     {
-    case 0: //temp
+        case 0: //voltage
         {
-            const float voltage = adc_read() * 3.3f / (1<<12);
-            const float tempC = 27.0f - (voltage - 0.706f) / 0.001721f;
-            printed = snprintf(pcInsert, iInsertLen, "%f", tempC);
+            
+            float voltage = calculate_voltage(adc_read());
+            printed = snprintf(pcInsert, iInsertLen, "%.2f", voltage);
+
+        }
+        break;
+
+
+
+
+        case 1: //temp
+        {
+
+            float temperature = calculate_temperature(calculate_resistance(calculate_voltage(adc_read())));
+            printed = snprintf(pcInsert, iInsertLen, "%.2f", temperature);
+            
+        }
+        break;
+
+
+        case 2: //Resistance
+        {
+
+            float resistance = calculate_resistance(calculate_voltage(adc_read()));
+            printed = snprintf(pcInsert, iInsertLen, "%.2f", resistance);
             
         }
         break;
     
-    default:
+        case 3: //ADC
+        {
+
+            
+            printed = snprintf(pcInsert, iInsertLen, "%i", adc_read());
+            
+        }
+        break;
+
+        default:
+        {
         printed = 0;
         break;
+        }
     }
 
 return(u16_t)printed;
@@ -35,8 +69,7 @@ return(u16_t)printed;
 void ssi_init(){
     //initialise adc
     adc_init();
-    adc_set_temp_sensor_enabled(true);
-    adc_select_input(4);
+    adc_select_input(0);
 
 
     //configure SSI handler
